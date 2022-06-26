a7d0bbe3bf2242ef28f4389afe12f8c0b6ad2f02
hinge-moto <drm1@ualberta.ca>
2022-06-26 14:40:08 -0600

    Basic styling added

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    # Third party apps.
+    'bootstrap4',
+
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_URL = 'users:login'
